b3b942c11532e88b55f2e58fc47f2d37af12eca5
patymtz17 <181730@upslp.edu.mx>
2025-09-03 19:35:53 -0600

    Update 03_query_faltas.sql

--- 03_query_faltas.sql
+++ 03_query_faltas.sql
@@ -1,6 +1,10 @@
-    SELECT g.periodo, g.seccion, g.nombre_grupo,
-           COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
-    FROM asistencia a
-    JOIN grupos g ON a.periodo = g.periodo AND a.seccion = g.seccion
-    GROUP BY g.periodo, g.seccion, g.nombre_grupo
-    ORDER BY g.seccion;
+SELECT 
+  g.periodo, 
+  g.seccion, 
+  g.nombre_grupo,
+  COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
+FROM asistencia a
+JOIN grupos g 
+  ON a.periodo = g.periodo AND a.seccion = g.seccion
+GROUP BY g.periodo, g.seccion, g.nombre_grupo
+ORDER BY g.seccion;
