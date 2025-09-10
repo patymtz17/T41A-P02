4c1899899a6fd17360d5958739546071cf27859d
patymtz17 <181730@upslp.edu.mx>
2025-09-09 18:30:59 -0600

    Update test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -13,37 +13,31 @@ def db_connection():
     yield conn
     conn.close()
 
-def test_alumno_insertado(db_connection):
+def test_alumnos_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM alumno;")
+        cur.execute("SELECT COUNT(*) FROM alumnos;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_cursos_insertado(db_connection):
-    with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM cursos;")
-        count = cur.fetchone()[0]
-        assert count == 10
-
-def test_maestros_insertado(db_connection):
+def test_maestros_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM maestros;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_grupos_insertado(db_connection):
+def test_grupos_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM grupos;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_inscripcion_insertado(db_connection):
+def test_inscripciones_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM inscripcion;")
+        cur.execute("SELECT COUNT(*) FROM inscripciones;")
         count = cur.fetchone()[0]
         assert count == 10  
 
-def test_asistencia_insertado(db_connection):
+def test_asistencia_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM asistencia;")
         count = cur.fetchone()[0]
@@ -52,15 +46,18 @@ def test_asistencia_insertado(db_connection):
 def test_structure(db_connection):
     sql = ''' 
             SELECT tablename FROM pg_tables 
-            WHERE tablename
-            IN ('alumno', 'cursos', 'maestros', 'grupos',
-                  'inscripcion', 'asistencia');
+            WHERE tablename 
+            IN ('alumnos', 'maestros', 'grupos',
+            'inscripciones', 'asistencia');
           '''
-    expected_tables = {'alumno', 'cursos', 'maestros', 'grupos',
-                       'inscripcion', 'asistencia'}
+    expected_tables = {'alumnos', 'maestros',
+                       'grupos', 'inscripciones',
+                       'asistencia'}
     with db_connection.cursor() as cur:
         cur.execute(sql)
         result_tables = {row[0] for row in cur.fetchall()}
-        msg = f'''Expected tables {expected_tables}, but found {result_tables}'''
-         assert expected_tables.issubset(result_tables)
-
+        msg = f'''Expected tables {expected_tables},
+              but found {result_tables}'''
+        assert result_tables == expected_tables, msg
+                
+        
